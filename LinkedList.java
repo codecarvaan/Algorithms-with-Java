dfd10a58bf3c89429a923f370c1b069ad31e63b5
shivam sharma <sharma.shivam7770@gmail.com>
2018-09-10 13:28:08 -0700

    first commit

---
+++ LinkedList.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class LinkedList{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String msg=sc.nextLine();
+        System.out.println("hello world"+msg);
+    }
+}
